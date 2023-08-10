047a2a319a30fa8bc4370eeb4409da9a244cf6c9
Rahul Kumar <rahulrai2822march@gmail.com>
2023-08-10 18:23:55 +0530

    updates

--- AdvancedJS/higherOrderFunction.js
+++ AdvancedJS/higherOrderFunction.js
@@ -2,7 +2,7 @@ const add = (a,b) => a+b
 const sub = (a,b) => Math.abs(a+b)
 const pro = (a,b) => a*b
 
-// HIGER ORDER FUNCTION
+// HIGER ORDER FUNCTIONs
 const calculator = (a , b , operator) => operator(a,b)
 
 console.log(calculator(5,6,pro))
